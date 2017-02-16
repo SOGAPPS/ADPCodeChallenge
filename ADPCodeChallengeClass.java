0ef0f01bcd9ee575377173a508a0bdd23bd0189f
Michael Adesegun <segunadesegun@yahoo.co.uk>
2017-02-16 13:33:19 -0500

    Initial Commit
    ADP Code Challenge

---
+++ ADPCodeChallengeClass.java
@@ -0,0 +1,25 @@
+package com.sogapps.sbdassessment.ADPCodeChallenge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.TreeMap;
+
+/**
+ * Created by AdesegunAdesegun on 06/01/2017.
+ */
+
+public class ADPCodeChallengeClass extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Run Test Code
+        // Call "" Method to run Function.
+
+    }
+
+
+
+}
